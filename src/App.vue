99cccc355dab392d7d7110c33fe021c966fa2fb7
Pavel <antonenko.pvl@gmail.com>
2019-06-29 22:37:15 +0300

    update demo

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,7 @@
       </v-layout>
       <v-layout align-center>
         <v-flex>
-          <code class="pa-4 text-xs-left">
-            this.$vDialog({
-              title: 'Confirm',
-              message: 'Are you sure?'
-            })
-            .onOk(() => {
-              console.log('Ok')
-            })
-            .onCancel(() => {
-              console.log('Cancel')
-            })
-          </code>
+          <code class="pa-4 text-xs-left">{{ code }}</code>
         </v-flex>
       </v-layout>
     </v-container>
@@ -32,17 +21,37 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      code: `
+      this.$vDialog({
+        title: 'Confirm',
+        message: 'Are you sure?',
+        okBtnText: 'Yes!',
+        cancelBtnText: 'Nooo!!!'
+      })
+      .onOk(() => {
+        console.log('Yes')
+      })
+      .onCancel(() => {
+        console.log('No')
+      })
+      `
+    }
+  },
   methods: {
     showDialog() {
       this.$vDialog({
         title: 'Confirm',
-        message: 'Are you sure?'
+        message: 'Are you sure?',
+        okBtnText: 'Yes!',
+        cancelBtnText: 'Nooo!!!'
       })
         .onOk(() => {
-          console.log('Ok')
+          console.log('Yes')
         })
         .onCancel(() => {
-          console.log('Cancel')
+          console.log('No')
         })
     }
   }
