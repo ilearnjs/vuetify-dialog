9597c48fb4f6090c773667e7e51cc4173aef0ef6
Pavel <antonenko.pvl@gmail.com>
2019-06-29 21:33:47 +0300

    add demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-container text-xs-center>
+      <v-layout align-center>
+        <v-flex>
+          <v-btn
+            @click="showDialog"
+            class="ma-4"
+          >Click me</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center>
+        <v-flex>
+          <code class="pa-4 text-xs-left">
+            this.$vDialog({
+              title: 'Confirm',
+              message: 'Are you sure?'
+            })
+            .onOk(() => {
+              console.log('Ok')
+            })
+            .onCancel(() => {
+              console.log('Cancel')
+            })
+          </code>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
-  mounted() {
-    this.$vDialog({
-      title: 'Confirm',
-      message: 'Are you sure?'
-    })
-      .onOk(() => console.log('Ok'))
-      .onCancel(() => console.log('Cancel'))
+  methods: {
+    showDialog() {
+      this.$vDialog({
+        title: 'Confirm',
+        message: 'Are you sure?'
+      })
+        .onOk(() => {
+          console.log('Ok')
+        })
+        .onCancel(() => {
+          console.log('Cancel')
+        })
+    }
   }
 }
 </script>
